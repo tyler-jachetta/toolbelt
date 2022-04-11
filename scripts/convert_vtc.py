601f13b45a4d36ba48088b06c94fffd954f007d8
Tyler Jachetta <tjachetta@imgix.com>
2022-04-11 10:30:14 -0600

    bit more work on this pretty much dead script

--- scripts/convert_vtc.py
+++ scripts/convert_vtc.py
@@ -4,7 +4,6 @@ import argparse
 import collections
 import logging
 import pathlib
-import sys
 import yaml
 
 import re
@@ -20,10 +19,11 @@ TAVERN_EXT = '.tavern.yaml'
 TEST_RE_STR = r'^\s*varnishtest\s*"(?P<test_name>.*?)"\n(?P<test_content>.*)$'
 
 # GMXS
-#CLIENT_RE_STR = r'\nclient\s(?P<client_name>\S*?)\s*{(?P<body>.*?)}\s*(?:-run)?'
-CLIENT_RE_STR = r'^\s*client\s(?P<client_name>\S*?)\s*{\s*(?:#\s*(?P<comment>.*?)$)?(?P<body>.*?)}\s*(?:-run)?'
+CLIENT_RE_STR = r'^(?:#(?P<above_comment>[^\n]*)\n)?client\s(?P<client_name>\S*?)\s*{\s*(?:#\s*(?P<below_comment>.*?)$)?(?P<body>.*?)^}\s*(?:-run)?'
 # GMX
 CASE_RE_STR = r'txreq\s+-req\s+"(?P<method>[A-Z]+)"\s+-url\s+"(?P<url>[/a-z-A-Z_0-9\.]+)"(?P<headers>(?:\s*\\\n\s*-hdr\s*".*?")*)\s*rxresp\s*\n(?P<response>(?:^\s*expect.*?$)*)\s*\Z'
+#CASE_RE_STR = r'txreq\s.*?
+TX_RE_STR = r'txreq\s+-req\s+(")?(?P<method>[A-Z]+"?)(?(1)\1)\s+-url\s+(")?(?P<url>[/a-z-A-Z_0-9\.]+)(?(3)\3)'
 
 # GM
 HEADER_RE_STR = r'-hdr\s*"(?P<key>.*?):\s*(?P<val>.*?)"(:?\s*\\)?$'
@@ -35,6 +35,11 @@ CLIENT_RE = re.compile(CLIENT_RE_STR, re.M | re.S)
 CASE_RE = re.compile(CASE_RE_STR, re.M | re.S)
 HEADER_RE = re.compile(HEADER_RE_STR, re.M)
 EXPECTATION_RE = re.compile(EXPECTATION_RE_STR, re.M)
+TX_RE = re.compile(TX_RE_STR)
+
+CURLY_BRACKET_ESC_RES = [
+    ('{{', re.compile(r'(?<!\{)\{(?!\{)')),
+    ('}}', re.compile(r'(?<!\})\}(?!\})'))]
 
 # for yaml keys
 STAGES = 'stages'
@@ -76,13 +81,29 @@ def _convert_header(key, val):
     elif key == "X-IXSource-AccountID" and val == LOTSA_ZEROS:
         return (key, '{account_id:s}')
 
+    if '{' in val:
+        print("FOUND IT")
+
+    for replacement, esc_re in CURLY_BRACKET_ESC_RES:
+        match = esc_re.search(val)
+        if match is None:
+            continue
+
+        val = esc_re.sub(replacement, val)
+
     return key, val
+
+
+def _join_split_lines(in_text):
+    return in_text.replace('\\\n', '')
+
 
 def _string_stripped(inval):
     outval = str(inval)
     if len(outval) > 1 and outval[-1] == outval[0] and outval[0] in ['"', "'"]:
         outval = outval[1:-1]
     return outval
+
 
 def _convert_response(response_vtc):
     response = {}
@@ -181,15 +202,23 @@ stages:
     test_definition = {'test_name': test_name, 'stages': stages}
 
     # parse individual cases
-
     for client in CLIENT_RE.finditer(test_content):
         this_iter = 0
         client_case_name = None
-        if client.groupdict().get('comment'):
-            client_case_name = client.groupdict()['comment']
+        gd = client.groupdict()
+        # the comments fight it out
+        if gd.get('above_comment') and gd.get('below_comment'):
+            import pdb
+            pdb.set_trace()
+            raise ValueError("I'm just not built for this")
+        elif gd.get('above_comment') or gd.get('below_comment'):
+            client_case_name = gd.get('below_comment', gd.get('above_comment'))
         else:
             client_case_name = f'{test_name}-{client.groupdict()["client_name"]}'
 
+        if not CASE_RE.search(client.groupdict()['body']):
+            import pdb
+            pdb.set_trace()
         for case in CASE_RE.finditer(client.groupdict()['body']):
             case_name = client_case_name
             if this_iter:
@@ -200,6 +229,9 @@ stages:
 
             stages.append(Stage(name=case_name, request=request, response=response))
             this_iter += 1
+    if not stages:
+        import pdb
+        pdb.set_trace()
     with dest.open('w') as dest_fh:
         yaml.safe_dump(test_definition, dest_fh, sort_keys=False)
 
